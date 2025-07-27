2178aab73cbf25c3675029f1e7a1ea89acde4572
Arin7712 <aringawande7712@gmail.com>
2025-07-27 16:31:44 +0530

    first one

---
+++ components/Hero.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { MoveRight, Star } from "lucide-react";
+import { Button } from "./ui/button";
+import { AnimatedGridPattern } from "./magicui/animated-grid-pattern";
+import { cn } from "@/lib/utils";
+import { CoverDemo } from "./CoverDemo";
+import { Cover } from "./ui/cover";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="relative md:px-0 px-6 flex flex-col items-center justify-center text-center h-screen md:h-[95vh] text-neutral-800">
+      <AnimatedGridPattern
+        numSquares={20}
+        maxOpacity={0.1}
+        duration={3}
+        repeatDelay={1}
+        className={cn(
+          "absolute inset-0 w-full h-full opacity-75",
+          "[mask-image:linear-gradient(to_bottom,white_52%,transparent)]",
+          "mask-size-100 mask-repeat-no-repeat",
+          "pointer-events-none"
+        )}
+      />
+      <div className="space-y-5 md:max-w-3xl text-center flex flex-col justify-center items-center z-10 ">
+        <div className="flex items-center justify-center gap-4 border-[1px] rounded-full bg-neutral-100 px-4 py-1">
+          <h1 className="text-sm">Trusted by 10+ B2B service providers</h1>
+          <div className="flex gap-1">
+            <Star className="fill-yellow-300 text-yellow-300 size-4" />
+            <Star className="fill-yellow-300 text-yellow-300 size-4" />
+            <Star className="fill-yellow-300 text-yellow-300 size-4" />
+            <Star className="fill-yellow-300 text-yellow-300 size-4" />
+            <Star className="fill-yellow-300 text-yellow-300 size-4" />
+          </div>
+        </div>
+        <h1 className="text-4xl md:text-[2.8rem] font-semibold">
+          {/* We Help B2B Services Drive More Leads On <Cover>LinkedIn</Cover> */}
+          Targeted LinkedIn Client Aquisiton That Converts <Cover>More Leads</Cover>
+        </h1>
+        <p className="md:text-lg text-md text-neutral-500 max-w-2xl">
+          We help B2B service providers consistently{" "}
+          <span className="">book qualified sales calls </span>
+          through targeted LinkedIn client acquisition frameworks.
+        </p>
+        <div className="flex items-center justify-center gap-6 relative">
+          <Button className="bg-indigo-500 px-2 text-md py-6 rounded-full uppercase">
+            {" "}
+            Book a Free Call
+            <MoveRight className="bg-white p-2 rounded-full text-indigo-500 size-8" />
+          </Button>
+          <Image
+            src="/hero-vector.svg"
+            alt="hero-vector"
+            width={80}
+            height={80}
+            className="absolute left-[110%] bottom-2"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { navItems } from '@/constants'
+import { Button } from './ui/button'
+
+const Navbar = () => {
+  return (
+    <nav className='absolute w-full flex items-center justify-between py-4 px-6 md:px-[6rem] bg-neutral-900 text-white z-10'>
+      <div>
+        <h1 className='text-2xl font-bold'>Acquireon</h1>
+      </div>
+
+      <div className='md:block hidden'>
+        <ul className='flex gap-6'>
+        {
+            navItems.map((item,i) => (
+                <li key={i} className='text-sm'>
+                    {item.name}
+                </li>
+            ))
+        }
+        </ul>
+      </div>
+
+      <div>
+        <Button className='bg-indigo-500 rounded-full px-6 uppercase'>Contact us</Button>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
